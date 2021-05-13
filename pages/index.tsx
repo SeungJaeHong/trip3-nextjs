a450d5cd7e1114bc9f03d71c8592329a067eb4e7
kkallasm <kaspar68@gmail.com>
2021-05-13 20:16:50 +0300

    tailwind added

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <div className="p-6 pt-12 lg:container lg:mx-auto bg-gray-50 bg-green-200">
+            Hello world
+        </div>
+    )
+}
