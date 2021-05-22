d8e56e9041d228e5d65882f2dbcee37569ae25d4
kkallasm <kaspar68@gmail.com>
2021-05-23 01:30:44 +0300

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div className="w-100 bg-no-repeat bg-center bg-cover" style={{ backgroundImage: 'url("/images/bg.jpg")' }}>
             <div className="p-6 pt-12 lg:container lg:mx-auto h-96">
-                ASDF
+                ASDF123
             </div>
         </div>
     )
