ee90165ec82b8b1476028ca5454e0ebe0f481274
kkallasm <kaspar68@gmail.com>
2021-07-26 15:16:14 +0300

    forum post likes

--- src/components/Forum/ForumComment/index.tsx
+++ src/components/Forum/ForumComment/index.tsx
@@ -3,6 +3,9 @@ import styles from './ForumComment.module.scss'
 import {Comment} from "../../../types"
 import ReactMarkdown from "react-markdown"
 import UserAvatar from "../../User/UserAvatar"
+import ThumbsUpIcon from "../../../icons/ThumbsUpIcon";
+import ThumbsDownIcon from "../../../icons/ThumbsDownIcon";
+import clsx from "clsx";
 
 const ForumComment = (item: Comment) => {
     return (
@@ -20,6 +23,22 @@ const ForumComment = (item: Comment) => {
             </div>
             <div className={styles.Body}>
                 <ReactMarkdown>{item.body}</ReactMarkdown>
+            </div>
+            <div className={styles.Actions}>
+                <div className={styles.Buttons}>
+                    <span className={styles.ActionButton}>Muuda</span>/
+                    <span className={styles.ActionButton}>Peida</span>
+                </div>
+                <div className={styles.Thumbs}>
+                    <div className={styles.Thumb}>
+                        <ThumbsUpIcon />
+                        <span className={styles.ThumbsCount}>{item.likes}</span>
+                    </div>
+                    <div className={clsx(styles.Thumb, styles.ThumbDown)}>
+                        <ThumbsDownIcon />
+                        <span className={styles.ThumbsCount}>{item.dislikes}</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
