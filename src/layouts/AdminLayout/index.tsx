ded7c178711230f51e1133693b584d1c04866547
kkallasm <kaspar68@gmail.com>
2021-11-16 20:04:04 +0200

    adminlayout css

--- src/layouts/AdminLayout/index.tsx
+++ src/layouts/AdminLayout/index.tsx
@@ -3,6 +3,7 @@ import styles from './AdminLayout.module.scss'
 import Footer from "../../components/Footer"
 import SidebarMenu from "../../components/Admin/SidebarMenu"
 import Navbar from "../../components/Navbar"
+import containerStyle from "../../styles/containers.module.scss"
 
 type Props = {
     title: string
@@ -11,7 +12,7 @@ type Props = {
 
 const AdminLayout = ({title, children}: Props) => {
     return (
-        <>
+        <div className={containerStyle.ContainerXl}>
             <div className={styles.AdminLayout}>
                 <div className={styles.SidebarMenu}>
                     <SidebarMenu />
@@ -33,7 +34,7 @@ const AdminLayout = ({title, children}: Props) => {
             <div className={styles.Footer}>
                 <Footer simple={true} />
             </div>
-        </>
+        </div>
     )
 }
 
