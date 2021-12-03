a546041787f6eeba1bc6695ebab07b383b576d50
kkallasm <kaspar68@gmail.com>
2021-12-03 23:36:29 +0200

    user profile update

--- src/components/User/UserEditForm/index.tsx
+++ src/components/User/UserEditForm/index.tsx
@@ -1,6 +1,5 @@
-import React, {useEffect} from "react"
+import React from "react"
 import styles from "./UserEditForm.module.scss"
-import clsx from "clsx"
 import Router from "next/router"
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -10,7 +9,10 @@ import useUser from "../../../hooks"
 import FormInput from "../../Form/FormInput"
 import SubmitButton from "../../Form/SubmitButton"
 import {UserProfile} from "../../../types"
-import FormTextarea from "../../Form/FormTextarea";
+import FormTextarea from "../../Form/FormTextarea"
+import FormRadioButton from "../../Form/FormRadioButton"
+import {updateUserProfile} from "../../../services/user.service"
+import {setFormErrors} from "../../../helpers"
 
 type Inputs = {
     name: string,
@@ -18,6 +20,12 @@ type Inputs = {
     password: string
     password_confirmation: string
     description: string
+    gender: number
+    birthyear: number
+    facebook: string
+    instagram: string
+    twitter: string
+    homepage: string
 }
 
 const UserEditForm = (userProfile: UserProfile) => {
@@ -25,49 +33,62 @@ const UserEditForm = (userProfile: UserProfile) => {
     const registerSchema = yup.object().shape({
         name: yup.string().required('Kasutajanimi on kohustuslik'),
         email: yup.string().email('E-post ei ole korrektne').required('E-post on kohustuslik'),
-        password: yup.string().required('Parool on kohustuslik'),
+        password: yup.string(),
         password_confirmation: yup.string()
-            .required('Parooli kordamine on kohustuslik')
             .oneOf([yup.ref('password'), null], 'Paroolid ei ühti'),
+        birthyear: yup.number().max(new Date().getFullYear(), 'Valesti sisestatud aasta').min(1900, 'Valesti sisestatud aasta'),
+        facebook: yup.string().url('Ei ole korrektne url'),
+        instagram: yup.string().url('Ei ole korrektne url'),
+        twitter: yup.string().url('Ei ole korrektne url'),
+        homepage: yup.string().url('Ei ole korrektne url')
     }).required()
 
     const { register, handleSubmit, setError, formState: { errors, isSubmitting } } = useForm<Inputs>({
-        resolver: yupResolver(registerSchema)
+        resolver: yupResolver(registerSchema),
+        defaultValues: {
+            name:userProfile.name,
+            email: userProfile.email,
+            description: userProfile.description,
+            gender: userProfile.gender ? userProfile.gender : undefined,
+            birthyear: userProfile.birthYear,
+            facebook: userProfile.contact_facebook,
+            instagram: userProfile.contact_instagram,
+            twitter: userProfile.contact_twitter,
+            homepage: userProfile.contact_homepage,
+        },
+        criteriaMode: "all",
+        shouldFocusError: true
     })
 
-    const handleRegister: SubmitHandler<Inputs> = async (values: Inputs) => {
-        const { name, email, password } = values
-        /*const resp = await registerUser(name, email, password).then(res => {
-            Router.push('/login')
+    const handleUpdate: SubmitHandler<Inputs> = async (values: Inputs) => {
+
+        console.log(values)
+
+        const resp = await updateUserProfile(userProfile.id, values).then(res => {
+            //Router.push('/user/' + userProfile.id)
             toast.success(
-                'Kasutaja loomine õnnestus!',
+                'Profiili uuendamine õnnestus!',
                 {
-                    duration: 5000
-                }
-            )
-            toast.success(
-                'Palun kontrolli oma e-posti ja vii kasutaja registeerimine lõpuni',
-                {
-                    duration: 6000
+                    duration: 3000
                 }
             )
         }).catch(err => {
             if (err.response?.data?.errors) {
                 setFormErrors(err.response?.data?.errors, setError)
             }
-            toast.error('Registreerimine ebaõnnestus!')
-        })*/
+            toast.error('Profiili uuendamine ebaõnnestus!')
+        })
     }
 
     return (
         <div className={styles.UserEditForm}>
             <div className={styles.FormContainer}>
-                <form onSubmit={handleSubmit(handleRegister)}>
+                <form onSubmit={handleSubmit(handleUpdate)}>
                     <div className={styles.SubHeading}>
                         Profiilipilt
                     </div>
                     <div className={styles.FormInput}>
-                        Pilt
+                        Imageupload
                     </div>
                     <div className={styles.SubHeading}>
                         Kasutaja andmed
@@ -100,7 +121,6 @@ const UserEditForm = (userProfile: UserProfile) => {
                             label={'Parool'}
                             type={'password'}
                             disabled={isSubmitting}
-                            required={true}
                             error={errors.password?.message}
                             register={register} />
                     </div>
@@ -111,12 +131,51 @@ const UserEditForm = (userProfile: UserProfile) => {
                             label={'Parool uuesti'}
                             type={'password'}
                             disabled={isSubmitting}
-                            required={true}
                             error={errors.password_confirmation?.message}
                             register={register} />
                     </div>
                     <div className={styles.SubHeading}>
                         Üldinfo
+                    </div>
+                    <div className={styles.FormInput}>
+                        <div className={styles.TwoColField}>
+                            <div className={styles.GenderField}>
+                                <div className={styles.GenderLabel}>
+                                    Sugu
+                                </div>
+                                <div className={styles.GenderInputs}>
+                                    <FormRadioButton
+                                        id={'male'}
+                                        name={'gender'}
+                                        label={'Mees'}
+                                        type={'radio'}
+                                        value={'1'}
+                                        error={errors.gender?.message}
+                                        disabled={isSubmitting}
+                                        register={register} />
+
+                                    <FormRadioButton
+                                        id={'female'}
+                                        name={'gender'}
+                                        label={'Naine'}
+                                        type={'radio'}
+                                        value={'2'}
+                                        error={errors.gender?.message}
+                                        disabled={isSubmitting}
+                                        register={register} />
+                                </div>
+                            </div>
+                            <div className={styles.BirthYearField}>
+                                <FormInput
+                                    name={'birthyear'}
+                                    id={'birthyear'}
+                                    label={'Sünniaasta'}
+                                    type={'number'}
+                                    disabled={isSubmitting}
+                                    error={errors.birthyear?.message}
+                                    register={register} />
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.FormInput}>
                         <FormTextarea
@@ -127,6 +186,46 @@ const UserEditForm = (userProfile: UserProfile) => {
                             error={errors.description?.message}
                             register={register} />
                     </div>
+                    <div className={styles.SubHeading}>
+                        Kontaktinfo
+                    </div>
+                    <div className={styles.FormInput}>
+                        <FormInput
+                            name={'facebook'}
+                            id={'facebook'}
+                            label={'Facebook'}
+                            disabled={isSubmitting}
+                            error={errors.facebook?.message}
+                            register={register} />
+                    </div>
+                    <div className={styles.FormInput}>
+                        <FormInput
+                            name={'instagram'}
+                            id={'instagram'}
+                            label={'Instagram'}
+                            disabled={isSubmitting}
+                            error={errors.instagram?.message}
+                            register={register} />
+                    </div>
+                    <div className={styles.FormInput}>
+                        <FormInput
+                            name={'twitter'}
+                            id={'twitter'}
+                            label={'Twitter'}
+                            disabled={isSubmitting}
+                            error={errors.twitter?.message}
+                            register={register} />
+                    </div>
+                    <div className={styles.FormInput}>
+                        <FormInput
+                            name={'homepage'}
+                            id={'homepage'}
+                            label={'Homepage'}
+                            disabled={isSubmitting}
+                            error={errors.homepage?.message}
+                            register={register} />
+                    </div>
+
                     <div className={styles.SubmitButton}>
                         <SubmitButton
                             title={'Salvesta'}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,3 +4,7 @@ import {AxiosResponse} from "axios"
 export const getLastComments = async (id: number): Promise<AxiosResponse> => {
     return await ApiClient.get('/user/' + id + '/comments')
 }
+
+export const updateUserProfile = async (userId: number, values: any): Promise<AxiosResponse> => {
+    return await ApiClient.post('/user/' + userId + '/update', values)
+}
