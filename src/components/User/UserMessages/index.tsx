44ed9f85b321b5b0d663e02281922a2044a5e7a2
kkallasm <kaspar68@gmail.com>
2021-12-08 23:21:44 +0200

    user message logic moved from page to component

--- src/components/User/UserMessages/index.tsx
+++ src/components/User/UserMessages/index.tsx
@@ -1,10 +1,33 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import styles from "./UserMessages.module.scss"
 import {UserMessage} from "../../../types"
 import UserAvatar from "../UserAvatar"
 import Tag from "../../Tag"
+import {getMyMessages} from "../../../services/user.service"
+import LoadingSpinner2 from "../../LoadingSpinner2"
 
-const UserMessages = ({messages}: {messages: UserMessage[]}) => {
+const UserMessages = () => {
+    const [messages, setMessages] = useState<UserMessage[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        setLoading(true)
+        getMyMessages().then((response) => {
+            setMessages(response.data)
+            setLoading(false)
+        }).catch(err => {
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return (
+            <div className={styles.Loader}>
+                <LoadingSpinner2 />
+            </div>
+        )
+    }
+
     return (
         <div className={styles.UserMessages}>
             {messages.map(message => {
--- src/pages/profile/messages/index.tsx
+++ src/pages/profile/messages/index.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect, useState} from 'react'
+import React, {Fragment} from 'react'
 import styles from './UserProfileMessagesPage.module.scss'
 import containerStyle from '../../../styles/containers.module.scss'
 import useUser from "../../../hooks"
@@ -7,40 +7,14 @@ import BackgroundMap from "../../../components/BackgroundMap"
 import clsx from "clsx"
 import Navbar from "../../../components/Navbar"
 import Footer from "../../../components/Footer"
-import LoadingSpinner2 from "../../../components/LoadingSpinner2"
 import UserMessages from "../../../components/User/UserMessages"
-import {getMyMessages} from "../../../services/user.service"
-import {UserMessage} from "../../../types"
 
 const UserProfileMessagesPage = () => {
     const { userIsLoggedIn } = useUser()
-    const [messages, setMessages] = useState<UserMessage[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
     const router = useRouter()
-
-    useEffect(() => {
-        setLoading(true)
-        getMyMessages().then((response) => {
-            setMessages(response.data)
-            setLoading(false)
-        }).catch(err => {
-            setLoading(false)
-            if (err.response.status === 401) {
-                router.push('/')
-            }
-        })
-    }, [])
 
     if (userIsLoggedIn === false) {
         router.push('/')
-    }
-
-    if (loading) {
-        return (
-            <div className={styles.Loader}>
-                <LoadingSpinner2 />
-            </div>
-        )
     }
 
     return (
@@ -56,7 +30,7 @@ const UserProfileMessagesPage = () => {
                     </div>
                     <div className={clsx(containerStyle.ContainerLg, styles.UserMessagesContainer)}>
                         <div className={styles.Body}>
-                            <UserMessages messages={messages} />
+                            <UserMessages />
                         </div>
                     </div>
                 </div>
