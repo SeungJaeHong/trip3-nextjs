2f5af2eda17f3dd8b62d1a0bd307e61c4e873996
kkallasm <kaspar68@gmail.com>
2021-12-29 21:42:16 +0200

    imagegallery user image

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -21,12 +21,32 @@ const ImageGallery = ({images}: Props) => {
             >
                 {images.map(image => {
                     return (
-                        <a href={image.urlLarge} key={image.id} className={styles.Image}>
+                        <a href={image.urlLarge} key={image.id} className={styles.Image} data-sub-html={'#caption_' + image.id}>
                             <img alt={image.title} src={image.urlSmall} />
                         </a>
                     )
                 })}
             </LightGallery>
+
+            {images.map(image => {
+                return (
+                    <div id={'caption_' + image.id} style={{display:'none'}} key={'caption_' + image.id}>
+                        <h4 onClick={() => alert('jep')}>
+                            {image.title}
+                        </h4>
+                        {image?.user !== undefined &&
+                            <a href={'/user/' + image.user.id} className={styles.UserContainer}>
+                                {image.user.avatar &&
+                                    <div className={styles.UserAvatar}>
+                                        <img alt={image.user.name} src={image.user.avatar} />
+                                    </div>
+                                }
+                                <div className={styles.UserName}>{image.user.name}</div>
+                            </a>
+                        }
+                    </div>
+                )
+            })}
         </Fragment>
     )
 }
