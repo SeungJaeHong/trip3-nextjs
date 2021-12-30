3a038825b8f1db8f824fd42850d0de1f09059c4e
kkallasm <kaspar68@gmail.com>
2021-12-30 23:39:53 +0200

    lint

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -32,6 +32,7 @@ const ImageGallery = ({images, hideImage}: Props) => {
                 instance.destroyModules()
             })
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     if (images.length === 0) {
