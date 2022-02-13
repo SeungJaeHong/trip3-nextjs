d986769341a97d8b9e0ca9368c15d34db8c17955
kkallasm <kaspar68@gmail.com>
2022-02-14 00:00:17 +0200

    daterange picker init

--- src/components/Form/FormDatepicker/index.tsx
+++ src/components/Form/FormDatepicker/index.tsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './FormDatepicker.module.scss'
 import clsx from 'clsx'
-import 'react-widgets/styles.css'
-import DatePicker from 'react-widgets/DatePicker'
-import { format } from 'date-fns'
+import {addYears} from 'date-fns'
 import { et } from 'date-fns/locale'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+// @ts-ignore
+import { DateRangePicker } from 'react-date-range'
 
 // @ts-ignore
 const FormDatepicker = ({ id, name, label, type, error, onChange, register, ...props }: props) => {
-    console.log(register(name))
+    //console.log(register(name))
 
-    const onValueChange = (value: Date | null | undefined, rawValue: string) => {
-        console.log(value)
+    const [state, setState] = useState([
+        {
+            startDate : null,
+            endDate : new Date(''),
+            key: 'selection'
+        }
+    ]);
 
-        let formattedValue = null
+    const onValueChange = (value: any) => {
+        const selection = value.selection
+        if (selection) {
+            console.log(value.selection, 'value')
+            setState([selection])
+        }
+
+
+        /*let formattedValue = null
         if (value) {
             formattedValue = format(value, 'yyyy-MM-dd', {
                 locale: et,
             })
         }
 
-        console.log(formattedValue, 'formattedValue')
+        console.log(formattedValue, 'formattedValue')*/
 
         //this.props.onChange(value);
     }
@@ -33,16 +48,27 @@ const FormDatepicker = ({ id, name, label, type, error, onChange, register, ...p
         >
             {label !== undefined && <label htmlFor={props.id ?? props.name}>{label}</label>}
 
-            <DatePicker
-                id={id}
-                name={name}
-                containerClassName={'SomeClass'}
+            <DateRangePicker
                 onChange={onValueChange}
-                value={new Date('2022-03-12')}
-                //valueFormat={'yyyy-MM-dd'}
-                valueFormat={{ day: "numeric" , month: "numeric", year: "numeric", parse: "yyyy-MM-dd"}}
-                valueDisplayFormat={{ day: "numeric" , month: "numeric", year: "numeric" }}
-                //valueDisplayFormat={{ format: "yyyy-MM-dd" }}
+                //onRangeFocusChange={onRangeFocusChange}
+                showSelectionPreview={true}
+                moveRangeOnFirstSelection={false}
+                months={2}
+                ranges={state}
+                direction={'horizontal'}
+                showDateDisplay={true}
+                showPreview={true}
+                locale={et}
+                staticRanges={[]}
+                inputRanges={[]}
+                preventSnapRefocus={true}
+                dateDisplayFormat={'dd.MM.yyyy'}
+                monthDisplayFormat={'MMM yyyy'}
+                weekdayDisplayFormat={'EEEEE'}
+                dayDisplayFormat={'d'}
+                maxDate={addYears(new Date(), 10)}
+                minDate={addYears(new Date(), -1)}
+                className={styles.RangePicker}
             />
 
             {error?.length > 0 && <div className={styles.ErrorText}>{error}</div>}
