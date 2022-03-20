c6db304d98c62e580b72922763e8e361f6972cb7
kkallasm <kaspar68@gmail.com>
2022-03-20 11:52:07 +0200

    frontpage search css

--- src/components/FrontPageSearch/index.tsx
+++ src/components/FrontPageSearch/index.tsx
@@ -1,8 +1,29 @@
 import styles from './FrontPageSearch.module.scss'
-import MainSearchInput from "../MainSearchInput"
+import SearchIcon from "../../icons/SearchIcon"
+import LoadingSpinner from "../LoadingSpinner"
 
 const FrontPageSearch = () => {
-    return <MainSearchInput placeholder={'Kuhu sa täna tahaksid minna?'} />
+    return (
+        <div className={styles.FrontPageSearchContainer}>
+            <div className={styles.SearchInput}>
+                <div className={styles.Icon}>
+                    <SearchIcon width={'26'} height={'26'}/>
+                </div>
+                <input
+                    type="text"
+                    autoComplete="off"
+                    spellCheck={false}
+                    placeholder={'Kuhu sa täna tahaksid minna?'}
+                />
+                <div className={styles.LoadingSpinner}>
+                    <LoadingSpinner />
+                </div>
+            </div>
+            <div className={styles.SearchResults}>
+                Results
+            </div>
+        </div>
+    )
 }
 
 export default FrontPageSearch
