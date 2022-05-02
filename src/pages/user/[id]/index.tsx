d1853a24e6177e4e67ece77b1547d15dbee3fdf3
kkallasm <kaspar68@gmail.com>
2022-05-02 11:25:04 +0300

    ads to user page

--- src/pages/user/[id]/index.tsx
+++ src/pages/user/[id]/index.tsx
@@ -1,37 +1,39 @@
-import React, {Fragment} from 'react'
-import {GetServerSideProps} from 'next'
-import styles from "./UserPage.module.scss"
-import Header from "../../../components/Header"
-import Footer from "../../../components/Footer"
-import {UserPublicProfile} from "../../../types"
-import ApiClientSSR from "../../../lib/ApiClientSSR"
-import containerStyle from "../../../styles/containers.module.scss"
-import UserProfileAvatar from "../../../components/User/UserProfileAvatar"
-import DottedMapIcon from "../../../icons/DottedMapIcon"
-import ThumbsUpIcon from "../../../icons/ThumbsUpIcon"
-import PostIcon from "../../../icons/Admin/PostIcon"
-import CommentIcon from "../../../icons/CommentIcon"
-import ThumbsDownIcon from "../../../icons/ThumbsDownIcon"
-import PinIcon from "../../../icons/PinIcon"
-import StarIcon from "../../../icons/StarIcon"
-import Tag from "../../../components/Tag"
-import BlockTitle from "../../../components/BlockTitle"
-import UserLastComments from "../../../components/User/UserLastComments"
-import useUser from "../../../hooks"
-import {useRouter} from "next/router"
-import UserImageGallery from "../../../components/User/UserImageGallery"
+import React, { Fragment } from 'react'
+import { GetServerSideProps } from 'next'
+import styles from './UserPage.module.scss'
+import Header from '../../../components/Header'
+import Footer from '../../../components/Footer'
+import { UserPublicProfile } from '../../../types'
+import ApiClientSSR from '../../../lib/ApiClientSSR'
+import containerStyle from '../../../styles/containers.module.scss'
+import UserProfileAvatar from '../../../components/User/UserProfileAvatar'
+import DottedMapIcon from '../../../icons/DottedMapIcon'
+import ThumbsUpIcon from '../../../icons/ThumbsUpIcon'
+import PostIcon from '../../../icons/Admin/PostIcon'
+import CommentIcon from '../../../icons/CommentIcon'
+import ThumbsDownIcon from '../../../icons/ThumbsDownIcon'
+import PinIcon from '../../../icons/PinIcon'
+import StarIcon from '../../../icons/StarIcon'
+import Tag from '../../../components/Tag'
+import BlockTitle from '../../../components/BlockTitle'
+import UserLastComments from '../../../components/User/UserLastComments'
+import useUser from '../../../hooks'
+import { useRouter } from 'next/router'
+import UserImageGallery from '../../../components/User/UserImageGallery'
+import Ads from '../../../components/Ads'
 
 type Props = {
     userProfile: UserPublicProfile
 }
 
-const UserPage = ({userProfile}: Props) => {
+const UserPage = ({ userProfile }: Props) => {
     const { userIsLoggedIn, user } = useUser()
     const userIsOwner = userProfile.id === user?.id
     const userIsAdmin = userIsLoggedIn && user?.isAdmin
     const router = useRouter()
-    const showVisitedBlock = (userProfile.countriesVisited && userProfile.countriesVisited?.length > 0)
-        || (userProfile.citiesVisited && userProfile.citiesVisited?.length > 0)
+    const showVisitedBlock =
+        (userProfile.countriesVisited && userProfile.countriesVisited?.length > 0) ||
+        (userProfile.citiesVisited && userProfile.citiesVisited?.length > 0)
 
     const renderMessageBtn = () => {
         if (!userIsLoggedIn || userIsOwner) {
@@ -50,7 +52,8 @@ const UserPage = ({userProfile}: Props) => {
             <Header
                 backgroundImage={'/images/user_page_bg.jpg'}
                 className={styles.Header}
-                style={{backgroundPosition: '50% 25%'}} />
+                style={{ backgroundPosition: '50% 25%' }}
+            />
             <div className={styles.BodyContainer}>
                 <div className={containerStyle.ContainerLg}>
                     <div className={styles.Body}>
@@ -61,62 +64,57 @@ const UserPage = ({userProfile}: Props) => {
                             <UserProfileAvatar {...userProfile} />
                         </div>
                         <div className={styles.UserNameContainer}>
-                            <div className={styles.UserName}>
-                                {userProfile.name}
-                            </div>
+                            <div className={styles.UserName}>{userProfile.name}</div>
                             {renderMessageBtn()}
                         </div>
-                        <div className={styles.Joined}>
-                            Liitus Tripiga {userProfile.joinedDate}
-                        </div>
+                        <div className={styles.Joined}>Liitus Tripiga {userProfile.joinedDate}</div>
                         <div className={styles.Statistics}>
                             <div className={styles.StatisticsItem}>
                                 <ThumbsUpIcon />
-                                <span className={styles.StatisticsLabel}>
-                                    {userProfile.liked} meeldimist
-                                </span>
+                                <span className={styles.StatisticsLabel}>{userProfile.liked} meeldimist</span>
                             </div>
                             <div className={styles.StatisticsItem}>
                                 <ThumbsDownIcon />
-                                <span className={styles.StatisticsLabel}>
-                                    {userProfile.disliked} mittemeeldimist
-                                </span>
+                                <span className={styles.StatisticsLabel}>{userProfile.disliked} mittemeeldimist</span>
                             </div>
                             <div className={styles.StatisticsItem}>
                                 <PostIcon />
-                                <span className={styles.StatisticsLabel}>
-                                    {userProfile.postCount} postitust
-                                </span>
+                                <span className={styles.StatisticsLabel}>{userProfile.postCount} postitust</span>
                             </div>
                             <div className={styles.StatisticsItem}>
                                 <CommentIcon />
-                                <span className={styles.StatisticsLabel}>
-                                    {userProfile.commentCount} kommentaari
-                                </span>
+                                <span className={styles.StatisticsLabel}>{userProfile.commentCount} kommentaari</span>
                             </div>
                         </div>
 
-                        {showVisitedBlock &&
+                        {showVisitedBlock && (
                             <div className={styles.VisitedBlock}>
-                                <div className={styles.VisitedHeader}>
-                                    On külastanud:
-                                </div>
-                                {(userProfile.countriesVisited && userProfile.countriesVisited?.length > 0) &&
+                                <div className={styles.VisitedHeader}>On külastanud:</div>
+                                {userProfile.countriesVisited && userProfile.countriesVisited?.length > 0 && (
                                     <div className={styles.DestinationInfo}>
                                         <div className={styles.Info}>
                                             <PinIcon />
                                             <div className={styles.InfoTitle}>
-                                                {userProfile.countriesVisited.length} riiki ({userProfile.countryPercentage}%)
+                                                {userProfile.countriesVisited.length} riiki (
+                                                {userProfile.countryPercentage}%)
                                             </div>
                                         </div>
                                         <div className={styles.Tags}>
-                                            {userProfile.countriesVisited.map(destination => {
-                                                return <Tag title={destination.name} large={true} white={true} route={'/sihtkoht/' + destination.slug} key={destination.id} />
+                                            {userProfile.countriesVisited.map((destination) => {
+                                                return (
+                                                    <Tag
+                                                        title={destination.name}
+                                                        large={true}
+                                                        white={true}
+                                                        route={'/sihtkoht/' + destination.slug}
+                                                        key={destination.id}
+                                                    />
+                                                )
                                             })}
                                         </div>
                                     </div>
-                                }
-                                {(userProfile.citiesVisited && userProfile.citiesVisited?.length > 0) &&
+                                )}
+                                {userProfile.citiesVisited && userProfile.citiesVisited?.length > 0 && (
                                     <div className={styles.DestinationInfo}>
                                         <div className={styles.Info}>
                                             <PinIcon />
@@ -125,58 +123,82 @@ const UserPage = ({userProfile}: Props) => {
                                             </div>
                                         </div>
                                         <div className={styles.Tags}>
-                                            {userProfile.citiesVisited.map(destination => {
-                                                return <Tag title={destination.name} large={true} white={true} route={'/sihtkoht/' + destination.slug} key={destination.id} />
+                                            {userProfile.citiesVisited.map((destination) => {
+                                                return (
+                                                    <Tag
+                                                        title={destination.name}
+                                                        large={true}
+                                                        white={true}
+                                                        route={'/sihtkoht/' + destination.slug}
+                                                        key={destination.id}
+                                                    />
+                                                )
                                             })}
                                         </div>
                                     </div>
-                                }
+                                )}
                             </div>
-                        }
+                        )}
 
-                        {(userProfile.wantsToGo && userProfile.wantsToGo?.length > 0) &&
+                        {userProfile.wantsToGo && userProfile.wantsToGo?.length > 0 && (
                             <>
-                                <div className={styles.VisitedHeader}>
-                                    Tahab minna:
-                                </div>
+                                <div className={styles.VisitedHeader}>Tahab minna:</div>
                                 <div className={styles.DestinationInfo}>
                                     <div className={styles.Info}>
                                         <StarIcon />
-                                        <div className={styles.InfoTitle}>
-                                            {userProfile.wantsToGo.length} sihtkohta
-                                        </div>
+                                        <div className={styles.InfoTitle}>{userProfile.wantsToGo.length} sihtkohta</div>
                                     </div>
                                     <div className={styles.Tags}>
-                                        {userProfile.wantsToGo.map(destination => {
-                                            return <Tag title={destination.name} large={true} white={true} route={'/sihtkoht/' + destination.slug} key={destination.id} />
+                                        {userProfile.wantsToGo.map((destination) => {
+                                            return (
+                                                <Tag
+                                                    title={destination.name}
+                                                    large={true}
+                                                    white={true}
+                                                    route={'/sihtkoht/' + destination.slug}
+                                                    key={destination.id}
+                                                />
+                                            )
                                         })}
                                     </div>
                                 </div>
                             </>
-                        }
+                        )}
 
-                        {userIsLoggedIn &&
+                        {userIsLoggedIn && (
                             <div className={styles.ActionButtons}>
-                                {(userIsOwner || userIsAdmin) &&
-                                    <div className={styles.ActionButton} onClick={() => router.push('/user/' + userProfile.id + '/edit')}>
+                                {(userIsOwner || userIsAdmin) && (
+                                    <div
+                                        className={styles.ActionButton}
+                                        onClick={() => router.push('/user/' + userProfile.id + '/edit')}
+                                    >
                                         <span>{'Muuda profiili'}</span>
                                     </div>
-                                }
-                                {userIsOwner &&
+                                )}
+                                {userIsOwner && (
                                     <>
-                                        <div className={styles.ActionButton} onClick={() => router.push('/profile/messages')}>
+                                        <div
+                                            className={styles.ActionButton}
+                                            onClick={() => router.push('/profile/messages')}
+                                        >
                                             <span>{'Sõnumid'}</span>
                                         </div>
-                                        <div className={styles.ActionButton} onClick={() => router.push('/profile/destinations')}>
+                                        <div
+                                            className={styles.ActionButton}
+                                            onClick={() => router.push('/profile/destinations')}
+                                        >
                                             <span>{'Minu sihtkohad'}</span>
                                         </div>
-                                        <div className={styles.ActionButton} onClick={() => router.push('/user/' + userProfile.id + '/images')}>
+                                        <div
+                                            className={styles.ActionButton}
+                                            onClick={() => router.push('/user/' + userProfile.id + '/images')}
+                                        >
                                             <span>{'Minu pildid'}</span>
                                         </div>
                                     </>
-                                }
+                                )}
                             </div>
-                        }
+                        )}
                     </div>
                 </div>
             </div>
@@ -197,7 +219,10 @@ const UserPage = ({userProfile}: Props) => {
                             </div>
                         </div>
                         <div className={styles.Sidebar}>
-                            Sidebar
+                            <div className={styles.Ads}>
+                                <Ads type={'sidebar-small'} />
+                                <Ads type={'sidebar-large'} />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -215,18 +240,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
         if (!response.data) {
             return {
-                notFound: true
+                notFound: true,
             }
         }
 
         return {
             props: {
-                userProfile: response.data
-            }
+                userProfile: response.data,
+            },
         }
     } catch (e) {
         return {
-            notFound: true
+            notFound: true,
         }
     }
 }
