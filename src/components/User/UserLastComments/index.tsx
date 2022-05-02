1f8a212df230994d65b65a20c52b1a245372b5fb
kkallasm <kaspar68@gmail.com>
2022-05-02 11:52:30 +0300

    user latest comments

--- src/components/User/UserLastComments/index.tsx
+++ src/components/User/UserLastComments/index.tsx
@@ -1,9 +1,9 @@
-import styles from "./UserLastComments.module.scss"
-import {useEffect, useState} from "react";
-import {getLastComments} from "../../../services/user.service";
-import {UserComment, UserPublicProfile} from "../../../types";
-import LoadingSpinner2 from "../../LoadingSpinner2"
-import ForumComment from "../../Forum/ForumComment";
+import styles from './UserLastComments.module.scss'
+import { useEffect, useState } from 'react'
+import { getLastComments } from '../../../services/user.service'
+import { UserComment, UserPublicProfile } from '../../../types'
+import LoadingSpinner2 from '../../LoadingSpinner2'
+import ForumComment from '../../Forum/ForumComment'
 import Link from 'next/link'
 
 const UserLastComments = (user: UserPublicProfile) => {
@@ -11,7 +11,7 @@ const UserLastComments = (user: UserPublicProfile) => {
     const [loading, setLoading] = useState<boolean>(false)
 
     const getContentTypeName = (type: string) => {
-        switch(type) {
+        switch (type) {
             case 'travelmate':
                 return 'Reisikaaslased'
             case 'buysell':
@@ -22,7 +22,10 @@ const UserLastComments = (user: UserPublicProfile) => {
                 return 'Vaba teema'
             case 'forum':
                 return 'Üldfoorum'
-            default: return ''
+            case 'news':
+                return 'Uudised'
+            default:
+                return ''
         }
     }
 
@@ -48,17 +51,15 @@ const UserLastComments = (user: UserPublicProfile) => {
 
     return (
         <div className={styles.UserLastComments}>
-            {comments?.map(comment => {
+            {comments?.map((comment) => {
                 return (
                     <div className={styles.ForumRow} key={comment.id}>
                         <div className={styles.PostTitle}>
                             <Link href={comment.content.url}>
-                                <a>
-                                    {comment.content.title}
-                                </a>
+                                <a>{comment.content.title}</a>
                             </Link>
                             <span className={styles.ContentType}>
-                                ({getContentTypeName(comment.content.type)})
+                                <i>{getContentTypeName(comment.content.type)}</i>
                             </span>
                         </div>
                         <ForumComment item={comment} />
