952eccba158f85e3b32d824cc97fbd9916905a88
kkallasm <kaspar68@gmail.com>
2022-05-12 22:17:55 +0300

    404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import styles from './ErrorPage.module.scss'
+import containerStyle from '../styles/containers.module.scss'
+
+export default function ErrorPage404() {
+    return (
+        <div className={styles.Container}>
+            <div className={containerStyle.ContainerXl}>
+                <div className={styles.Navbar}>
+                    <Navbar darkMode={true} />
+                </div>
+                <div className={containerStyle.ContainerLg}>
+                    <div className={styles.Content}>
+                        <h2>Ups...</h2>
+                        <p>
+                            Suure tõenäosusega on lehekülg liigutatud teise kohta. Proovi meie otsingut, et seda leida.
+                        </p>
+                        <p>Kui soovid minna meie avalehele, vajuta Trip.ee logole.</p>
+                    </div>
+                </div>
+            </div>
+            <Footer simple={true} />
+        </div>
+    )
+}
