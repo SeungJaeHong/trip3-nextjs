04dbde1976382e59664ad70cb8c30c21ef1280a6
kkallasm <kaspar68@gmail.com>
2022-05-14 20:17:10 +0300

    facebook login final

--- src/components/FacebookLogin/index.tsx
+++ src/components/FacebookLogin/index.tsx
@@ -1,29 +1,31 @@
-import React, {Fragment} from "react"
+import React, { Fragment } from 'react'
 import Script from 'next/script'
-import styles from "./FacebookLogin.module.scss"
-import {toast} from 'react-toastify'
-import {createUserOrLogin} from "../../services/auth.service"
-import useUser from "../../hooks"
+import styles from './FacebookLogin.module.scss'
+import { toast } from 'react-toastify'
+import { createUserOrLogin } from '../../services/auth.service'
+import useUser from '../../hooks'
 
 const FacebookLogin = () => {
-    const {mutate } = useUser()
+    const { mutate } = useUser()
     const signInFB = () => {
-        FB.login(function(response) {
-            if (response.status === 'connected') {
-                FB.api('/me?fields=id,email,name,picture.width(200).height(200)', function(userResponse: any) {
-
-                    console.log(userResponse, 'userResponse')
-
-                    const imageUrl = userResponse?.picture?.data?.url ? userResponse.picture.data.url : undefined
-                    createUserOrLogin(userResponse.name, userResponse.email).then(res => {
-                        mutate(res.data)
-                        toast.success('Tere, ' + res.data.name + '!')
-                    }).catch(err => {
-                        toast.error('Sisselogimine ebaõnnestus!')
+        FB.login(
+            function (response) {
+                if (response.status === 'connected') {
+                    FB.api('/me?fields=id,email,name,picture.width(200).height(200)', function (userResponse: any) {
+                        const imageUrl = userResponse?.picture?.data?.url ? userResponse.picture.data.url : undefined
+                        createUserOrLogin(userResponse.name, userResponse.email, imageUrl)
+                            .then((res) => {
+                                mutate(res.data)
+                                toast.success('Tere, ' + res.data.name + '!')
+                            })
+                            .catch((err) => {
+                                toast.error('Sisselogimine ebaõnnestus!')
+                            })
                     })
-                })
-            }
-        }, {scope: 'public_profile,email'})
+                }
+            },
+            { scope: 'public_profile,email' }
+        )
     }
 
     return (
@@ -33,16 +35,16 @@ const FacebookLogin = () => {
             </div>
 
             <Script
-                src={"https://connect.facebook.net/en_US/sdk.js"}
+                src={'https://connect.facebook.net/en_US/sdk.js'}
                 onLoad={() => {
-                    window.fbAsyncInit = function() {
+                    window.fbAsyncInit = function () {
                         FB.init({
-                            appId      : process.env.FACEBOOK_APP_ID,
-                            cookie     : false,
-                            xfbml      : true,
-                            version    : 'v13.0'
-                        });
-                    };
+                            appId: process.env.FACEBOOK_APP_ID,
+                            cookie: false,
+                            xfbml: true,
+                            version: 'v13.0',
+                        })
+                    }
                 }}
             />
         </Fragment>
